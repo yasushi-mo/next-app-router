69af75182069f0df3a4410990cf935f7e2e37758
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-10-29 09:31:13 +0900

    Add: ServerComponent calling api using axios

--- src/app/random-joke/server-component/page.tsx
+++ src/app/random-joke/server-component/page.tsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { Suspense } from "react";
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch data");
+  }
+};
+
+async function RandomJoke() {
+  const randomJokeObject = await fetchData();
+  return <p>{randomJokeObject.setup}</p>;
+}
+
 export default function Page() {
   return (
     <div>
       <h1>Random Joke</h1>
+      <Suspense fallback={<p>Loading..</p>}>
+        <RandomJoke />
+      </Suspense>
     </div>
   );
 }
