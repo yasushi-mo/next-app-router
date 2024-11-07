9cb93d325974336d9dd195ba827a97d221fa66fb
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-11-08 06:55:38 +0900

    Add: test target code

--- src/app/random-joke/client-component/page.tsx
+++ src/app/random-joke/client-component/page.tsx
@@ -1,1 +1,25 @@
-export default function Page() {}
+"use client";
+
+import useSWR from "swr";
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  return await res.json();
+};
+
+export default function Page() {
+  const { data } = useSWR<{
+    id: number;
+    type: string;
+    setup: string;
+    punchline: string;
+  }>("https://official-joke-api.appspot.com/random_joke", fetcher);
+
+  return (
+    <div>
+      <h3>{data?.type}</h3>
+      <p>{data?.setup}</p>
+      <p>{data?.punchline}</p>
+    </div>
+  );
+}
