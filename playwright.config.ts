5a8fec22ee509bee437a2f30825f3a0589b8853e
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-12-08 14:24:43 +0900

    Add: Video on

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    /** Record video for each test */
+    video: "on",
   },
 
   /* Configure projects for major browsers */
