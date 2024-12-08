65119b0f7b36ac35a7148d67957acc3f796c49af
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-12-08 14:35:18 +0900

    Update: Test case

--- src/__tests__/e2e/page.spec.ts
+++ src/__tests__/e2e/page.spec.ts
@@ -3,10 +3,22 @@ import test, { expect } from "@playwright/test";
 test("Home page should work as expected", async ({ page }) => {
   // Start from Home page
   await page.goto("http://localhost:3000/");
-  // Find page title
-  await expect(page.locator("h1")).toContainText("Home");
-  // Find an element with the text 'About' and click on it
-  await page.click("text=About");
+  // Find page heading
+  await expect(page.getByRole("heading", { name: "Home" })).toBeVisible();
+  // Find link  with the text 'About' and click on it
+  const linkToAbout = page.getByRole("link", { name: "About" });
+  await expect(linkToAbout).toBeVisible();
+  await linkToAbout.click();
+
   // The new URL should be "/about"
   await expect(page).toHaveURL("http://localhost:3000/about");
+  // Find page heading
+  await expect(page.getByRole("heading", { name: "About" })).toBeVisible();
+  // Find link  with the text 'Home' and click on it
+  const linkToHome = page.getByRole("link", { name: "Home" });
+  await expect(linkToHome).toBeVisible();
+  await linkToHome.click();
+
+  // The new URL should be "/"
+  await expect(page).toHaveURL("http://localhost:3000/");
 });
