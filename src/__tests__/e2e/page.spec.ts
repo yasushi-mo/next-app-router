32b58f3bdcd61c6155cda9a3e7fc21f948a61ede
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-12-08 14:54:08 +0900

    Update: Test case

--- src/__tests__/e2e/page.spec.ts
+++ src/__tests__/e2e/page.spec.ts
@@ -14,11 +14,4 @@ test("Home page should work as expected", async ({ page }) => {
   await expect(page).toHaveURL("http://localhost:3000/about");
   // Find page heading
   await expect(page.getByRole("heading", { name: "About" })).toBeVisible();
-  // Find link  with the text 'Home' and click on it
-  const linkToHome = page.getByRole("link", { name: "Home" });
-  await expect(linkToHome).toBeVisible();
-  await linkToHome.click();
-
-  // The new URL should be "/"
-  await expect(page).toHaveURL("http://localhost:3000/");
 });
